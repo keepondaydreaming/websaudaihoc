b4aedec6a75edc161f440618988cbb6e13bcf4d3
keepondaydreaming <hyouka266@gmail.com>
2021-10-31 22:45:45 +0700

    update

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,30 @@
 import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Body = () => {
     const [text, setText] = useState('')
     const [score, setScore] = useState(0)
 
     useEffect(() => {
-        const timeOutId = setTimeout(() => setScore(text), 400)
+        const timeOutId = setTimeout(() => inference(text), 400)
         return () => clearTimeout(timeOutId)
     }, [text])
+
+    const inference = (text) => {
+        axios({
+            method: 'post',
+            url: 'http://localhost:8000/inference',
+            data: {
+                excerpt: text
+            }
+        })
+        .then(response => {
+            console.log(response)
+            setScore(response.data)
+        }).catch(error => {
+            console.log(error.message)
+        })
+    }
     
     const handleChange = (text) => {
         setText(text)
