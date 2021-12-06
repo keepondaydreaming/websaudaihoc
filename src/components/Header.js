40f8b1f8330d47de8720c409b69e11669a15f448
trasua <hyouka266@gmail.com>
2021-12-06 19:01:42 +0700

    update README.md

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <div className='bg-blue-400 py-4 mb-32'>
             <p className='text-lg ml-4 text-gray-100 uppercase font-bold font-sans'>
-                Web Sau Dai Hoc
+                READABILITY
             </p>
         </div>
     )
